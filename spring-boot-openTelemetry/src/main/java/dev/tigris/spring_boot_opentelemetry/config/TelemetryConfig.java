0b0b0261acdeb09d6c63342f5259f2485d04b08b
bllexe <bilalykt21@gmail.com>
2025-03-13 15:12:56 +0300

    added spring-boot opentelemetry

---
+++ spring-boot-openTelemetry/src/main/java/dev/tigris/spring_boot_opentelemetry/config/TelemetryConfig.java
@@ -0,0 +1,20 @@
+package dev.tigris.spring_boot_opentelemetry.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+
+@Configuration
+public class TelemetryConfig {
+
+    @Bean
+    OtlpHttpSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
+        return OtlpHttpSpanExporter.builder()
+        .setEndpoint(url)
+        .build();
+    }
+
+    
+}
---
+++ spring-boot-openTelemetry/src/test/java/dev/tigris/spring_boot_openTelemetry/SpringBootOpenTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tigris.spring_boot_openTelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpenTelemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
